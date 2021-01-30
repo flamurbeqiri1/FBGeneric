06d19b839fd0f7df333fe9a33273443f5f086df4
Flamur Beqiri <flamurbeqiri1@gmail.com>
2021-01-30 15:12:22 +0100

    Initial project setup

---
+++ FBGeneric/FBGeneric.h
@@ -0,0 +1,18 @@
+//
+//  FBGeneric.h
+//  FBGeneric
+//
+//  Created by Flamur Beqiri on 30.1.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FBGeneric.
+FOUNDATION_EXPORT double FBGenericVersionNumber;
+
+//! Project version string for FBGeneric.
+FOUNDATION_EXPORT const unsigned char FBGenericVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FBGeneric/PublicHeader.h>
+
+
---
+++ FBGenericExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FBGenericExample
+//
+//  Created by Flamur Beqiri on 30.1.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
